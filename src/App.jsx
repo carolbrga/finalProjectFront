f3b7fb6cf2bd727a74f97f318e1a99ce886b29d0
rochinhaDev <you@example.com>
2023-09-09 11:40:24 -0300

    front

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,27 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/Navbar";
 import ProtectRoute from "./components/ProtectRoute";
-
+import HomePage from "./pages/HomePage";
 function App() {
-   return (
-      <div className="bg-gray-100 min-h-screen">
-         <Navbar />
-         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <Routes>
-               {/* Rotas que não devem ser protegidas */}
-               <Route path="/" element={<HomePage />} />
-               <Route path="/login" element={<LoginPage />} />
-               <Route path="/signup" element={<SignupPage />} />
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <Routes>
+          {/* Rotas que não devem ser protegidas */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
 
-               {/* Rota protegida */}
-               <Route
-                  path="/profile"
-                  element={<ProtectRoute Component={ProfilePage} />}
-               />
-            </Routes>
-         </div>
+          {/* Rota protegida */}
+          <Route
+            path="/profile"
+            element={<ProtectRoute Component={ProfilePage} />}
+          />
+        </Routes>
       </div>
-   );
+    </div>
+  );
 }
 
 export default App;
