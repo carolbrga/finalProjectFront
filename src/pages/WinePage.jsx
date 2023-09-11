77cc53e1c0c66c4fe70463aa140be6ebe667ac90
Carolina Braga <brgacarolina@gmail.com>
2023-09-11 19:22:27 -0400

    add pictures to wines

--- src/pages/WinePage.jsx
+++ src/pages/WinePage.jsx
@@ -26,14 +26,15 @@ console.log(wines);
 return (
     <>
 <h1 className="flex justify-start" >Vinhos:</h1>
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center " >
 {wines.map((wine) =>{
     return (
         <div className="bg-white rounded-lg shadow-sm p-2  ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-1 m-4 w-1/4" key={wine._id}>
-<h2>{wine.grape} - {wine.brand}</h2>
+<h2 className="mt-4 text-center text-2xl font-bold leading-9 text-gray-900">{wine.grape} - {wine.brand}</h2>
+<img  src={wine.photo}/>
 <p>{wine.origin}</p>
 <p>{wine.year}</p>
-<p>{wine.price}</p>
+<p>R$ {wine.price}</p>
 <div>
     <Link to={`/detalhesdovinho/${wine._id}`}>Ver detalhes</Link>
 </div>
