c2904676a8c182dfc579afb6c7040eef5e5e08e6
rochinhaDev <you@example.com>
2023-09-11 20:20:26 -0300

    adjust

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-import HomePage  from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/Navbar";
 import WineDetailPage from "./pages/WineDetailPage";
@@ -20,24 +20,31 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-       
 
-
-
-               {/* Rota protegida */}
-               <Route
-                  path="/profile"
-                  element={<ProtectRoute Component={ProfilePage} />}
-               />
-               <Route path="/todosvinhos" element={<ProtectRoute Component={WinePage}/>}/>
-               <Route path="/detalhesdovinho" element={<ProtectRoute Component={WineDetailPage}/>}/>
-               <Route path="/todospacotes" element={<ProtectRoute Component={PacksPage}/>}/>
-               <Route path="/detalhespacotes" element={<ProtectRoute Component={PacksDetailPage}/>}/>
-               
-            </Routes>
-         </div>
-
+          {/* Rota protegida */}
+          <Route
+            path="/profile"
+            element={<ProtectRoute Component={ProfilePage} />}
+          />
+          <Route
+            path="/todosvinhos"
+            element={<ProtectRoute Component={WinePage} />}
+          />
+          <Route
+            path="/detalhesdovinho/wine/:id_wine"
+            element={<ProtectRoute Component={WineDetailPage} />}
+          />
+          <Route
+            path="/todospacotes"
+            element={<ProtectRoute Component={PacksPage} />}
+          />
+          <Route
+            path="/detalhespacotes/packs/:id_pack"
+            element={<ProtectRoute Component={PacksDetailPage} />}
+          />
+        </Routes>
       </div>
+    </div>
   );
 }
 
