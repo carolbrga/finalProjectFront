086efdf7e97fc08f7967875c8e3567b4d3deb478
Carolina Braga <brgacarolina@gmail.com>
2023-09-11 19:26:29 -0400

    add pictures to wines

--- src/pages/WinePage.jsx
+++ src/pages/WinePage.jsx
@@ -32,9 +32,10 @@ function WinePage() {
               className="bg-white rounded-lg shadow-sm p-2  ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-1 m-4 w-1/4"
               key={wine._id}
             >
-              <h2>
+              <h2 className="mt-4 text-center text-2xl font-bold leading-9 text-gray-900">
                 {wine.grape} - {wine.brand}
               </h2>
+              <img  src={wine.photo}/>
               <p>{wine.origin}</p>
               <p>{wine.year}</p>
               <p>{wine.price}</p>
