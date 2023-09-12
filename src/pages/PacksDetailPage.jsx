42364da4df0eedbb5bee4904ab29656128a60def
Victor Pimenta <victor.usdevcontact@gmail.com>
2023-09-11 20:24:20 -0400

    packs page history done

--- src/pages/PacksDetailPage.jsx
+++ src/pages/PacksDetailPage.jsx
@@ -1,60 +1,60 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import api from "../axios/api";
 import { Link } from "react-router-dom";
 
-function PacksDetailPage(){
+function PacksDetailPage() {
+  const [packsDetail, setPacksDetail] = useState([]);
+  const params = useParams();
 
-         const [packsDetail, setPacksDetail] = useState([]);
-     
-         useEffect(() => {
-            async function getPacksDetail() {
-              try {
-                const response = await api.get("/packs/all");
-                console.log(response.data);
-                setPacksDetail(response.data);
-                
-              } catch (error) {
-                console.error(error);
-             
-              }
-            }
-            getPacksDetail();
-          }, []);
-     
-        console.log(packsDetail);
-     
+  useEffect(() => {
+    async function getPacksDetail() {
+      try {
+        const response = await api.get(`/packs/${params.id_packs}`);
+        console.log(response.data);
+        setPacksDetail(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPacksDetail();
+  }, []);
 
+  console.log(packsDetail);
 
-    return (
+  async function handlePacksHistory() {
+    try {
+      await api.post(`"/add-pack-history/${params.id_packs}`);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-        <div>
-            <h1>
-                Pacotes disponíveis
-            </h1>
+  return (
+    <div>
+      <h1>Pacotes disponíveis</h1>
 
-            <div>
-{packsDetail.map((packDetail) =>{
-    return (
-        <div key={packDetail._id}>
-<h2>{packDetail.title}</h2>
-<h3>{packDetail.price}</h3>
-<h3>{packDetail.type}</h3>
-<p>{packDetail.wines}</p>
-<p>{packDetail.description}</p>
-<p>{packDetail.origin}</p>
-<p>{packDetail.delivery}</p>
-<div>
-    <Link to={`/detalhesdopack/${packDetail._id}`}>Ver detalhes</Link>
-</div>
-        </div>
-    )
-})}
-
+      <div>
+        {packsDetail.map((packDetail) => {
+          return (
+            <div key={packDetail._id}>
+              <h2>{packDetail.title}</h2>
+              <h3>{packDetail.price}</h3>
+              <h3>{packDetail.type}</h3>
+              <p>{packDetail.wines}</p>
+              <p>{packDetail.description}</p>
+              <p>{packDetail.origin}</p>
+              <p>{packDetail.delivery}</p>
+              <div>
+                <button onClick={handlePacksHistory}>Favoritar</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-        </div>
-    )
+  );
 }
-
 
 export default PacksDetailPage;
