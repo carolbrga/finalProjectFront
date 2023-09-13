6d1cd48ec956992182362b5cf280958c165fd1c2
rochinhaDev <you@example.com>
2023-09-13 20:21:09 -0300

    edit profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -197,72 +197,73 @@ function ProfilePage() {
             alt="Foto do usuário"
           />
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 text-gray-900">
-            Histórico de Vinhos:
-          </h2>
-          <div>
-            {user.history_wine && user.history_wine.length > 0 ? (
-              user.history_wine.map((wine) => (
-                <div
-                  key={wine._id}
-                  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
-                >
-                  <p>Nome do Vinho: {wine.grape}</p>{" "}
-                  <p>Teor de Alcool: {wine.alcoholLevel}</p>
-                  <p>Marca: {wine.brand}</p>
-                  {isEditing ? null : (
-                    <button
-                      onClick={() => handleDeleteWine(wine._id)}
-                      className=" bg-amber-950 py-2 px-4 rounded-lg text-white hover:bg-amber-900"
-                    >
-                      Excluir
-                    </button>
-                  )}
-                </div>
-              ))
-            ) : (
-              <p>Nenhum histórico de vinho disponível.</p>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 text-gray-900">
-            Histórico de Pacotes de Vinhos:
-          </h2>
-          <div className="flex flex-col justify-center items-center bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2 space-between">
-            {user.history_pack && user.history_pack.length > 0 ? (
-              user.history_pack.map((pack) => (
-                <div
-                  key={pack._id}
-                  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
-                >
-                  Nome do Pacote: {pack.title}{" "}
-                  {isEditing ? null : (
-                    <div>
+        {isEditing ? null : (
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 text-gray-900">
+              Histórico de Vinhos:
+            </h2>
+            <div>
+              {user.history_wine && user.history_wine.length > 0 ? (
+                user.history_wine.map((wine) => (
+                  <div
+                    key={wine._id}
+                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
+                  >
+                    <p>Nome do Vinho: {wine.grape}</p>{" "}
+                    <p>Teor de Alcool: {wine.alcoholLevel}</p>
+                    <p>Marca: {wine.brand}</p>
+                    {isEditing ? null : (
                       <button
-                        onClick={() => handleDeletePack(pack._id)}
+                        onClick={() => handleDeleteWine(wine._id)}
                         className=" bg-amber-950 py-2 px-4 rounded-lg text-white hover:bg-amber-900"
                       >
                         Excluir
                       </button>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <p>Nenhum histórico de vinho disponível.</p>
+              )}
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 text-gray-900">
+                Histórico de Pacotes de Vinhos:
+              </h2>
+              <div className="flex flex-col justify-center items-center bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2 space-between">
+                {user.history_pack && user.history_pack.length > 0 ? (
+                  user.history_pack.map((pack) => (
+                    <div
+                      key={pack._id}
+                      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
+                    >
+                      Nome do Pacote: {pack.title}{" "}
+                      {isEditing ? null : (
+                        <div>
+                          <button
+                            onClick={() => handleDeletePack(pack._id)}
+                            className=" bg-amber-950 py-2 px-4 rounded-lg text-white hover:bg-amber-900"
+                          >
+                            Excluir
+                          </button>
+                        </div>
+                      )}
                     </div>
-                  )}
-                </div>
-              ))
-            ) : (
-              <p>Nenhum histórico de pacote de vinho disponível.</p>
-            )}
+                  ))
+                ) : (
+                  <p>Nenhum histórico de pacote de vinho disponível.</p>
+                )}
+              </div>
+              <Link
+                to="/todosvinhos"
+                className="bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
+              >
+                Escolha seus vinhos
+              </Link>
+            </div>
           </div>
-        </div>
+        )}
       </div>
-
-      <Link
-        to="/todosvinhos"
-        className="bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
-      >
-        Escolha seus vinhos
-      </Link>
     </div>
   );
 }
