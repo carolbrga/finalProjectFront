b4621b13754a7e695e0e79217c2f1f17de5d0e7f
rochinhaDev <you@example.com>
2023-09-12 22:37:57 -0300

    form

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -28,15 +28,17 @@ function ProfilePage() {
     }
     getProfile();
   }, []);
+
   async function handleSubmitProfile(e) {
     e.preventDefault();
     try {
-      await api.put("/user/edit", formProfile);
+      await api.put(`/user/edit/${id_user}`, formProfile);
       setIsEditing(false);
     } catch (error) {
       console.log(error);
     }
   }
+
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormProfile({
@@ -44,22 +46,23 @@ function ProfilePage() {
       [name]: value,
     });
   }
-  async function handleDeleteWine(e) {
+
+  async function handleDeleteWine(id_wine) {
     try {
       await api.delete(`/wine/remove-wine-history/${id_wine}`);
-      setReload(!reload);
     } catch (error) {
       console.log(error);
     }
   }
-  async function handleDeletePack(e) {
+
+  async function handleDeletePack(id_pack) {
     try {
       await api.delete(`/packs/delete/${id_pack}`);
-      setReload(!reload);
     } catch (error) {
       console.log(error);
     }
   }
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center">
@@ -67,13 +70,112 @@ function ProfilePage() {
           PROFILE
         </h1>
         <div className="bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2">
-          <h1>Nome: {user.name}</h1>
-          <p>Email: {user.email}</p>
-          <p>CPF: {user.cpf}</p>
-          <p>Telefone: {user.telephone}</p>
-          <p>Idade: {user.age}</p>
-          <p>Endereço: {user.address}</p>
-          <button onClick={handleSubmitProfile}>Salvar</button>
+          {isEditing ? (
+            <form onSubmit={handleSubmitProfile}>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Nome:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formProfile.name}
+                  onChange={handleInputChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formProfile.email}
+                  onChange={handleInputChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="cpf"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  CPF:
+                </label>
+                <input
+                  type="text"
+                  id="cpf"
+                  name="cpf"
+                  value={formProfile.cpf}
+                  onChange={handleInputChange}
+                  className="block text-gray-700 font-bold mb-2"
+                />
+                <label
+                  htmlFor="age"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Idade:
+                </label>
+                <input
+                  type="text"
+                  id="age"
+                  name="age"
+                  value={formProfile.age}
+                  onChange={handleInputChange}
+                  className="block text-gray-700 font-bold mb-2"
+                />
+                <label
+                  htmlFor="telephone"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Telefone:
+                </label>
+                <input
+                  type="text"
+                  id="telephone"
+                  name="telephone"
+                  value={formProfile.telephone}
+                  onChange={handleInputChange}
+                  className="block text-gray-700 font-bold mb-2"
+                />
+                <label
+                  htmlFor="address"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Endereço:
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  value={formProfile.address}
+                  onChange={handleInputChange}
+                  className="block text-gray-700 font-bold mb-2"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Salvar
+              </button>
+            </form>
+          ) : (
+            <div>
+              <h1>Nome: {user.name}</h1>
+              <p>Email: {user.email}</p>
+              <button onClick={() => setIsEditing(true)}>Editar</button>
+            </div>
+          )}
         </div>
         <div>
           <img
@@ -94,9 +196,13 @@ function ProfilePage() {
                   className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-white rounded-lg shadow-sm p-2 ring-1 ring-offset-2 ring-gray-200 transform hover:scale-95 transition-transform duration-300 mb-2"
                 >
                   <p>Nome do Vinho: {wine.grape}</p>{" "}
-                  <p>Teor de Alcool:{wine.alcoholLevel}</p>
+                  <p>Teor de Alcool: {wine.alcoholLevel}</p>
                   <p>Marca: {wine.brand}</p>
-                  <button onClick={handleDeleteWine}>Excluir</button>
+                  {isEditing ? null : (
+                    <button onClick={() => handleDeleteWine(wine._id)}>
+                      Excluir
+                    </button>
+                  )}
                 </div>
               ))
             ) : (
@@ -113,9 +219,11 @@ function ProfilePage() {
               user.history_pack.map((pack) => (
                 <div key={pack._id}>
                   Nome do Pacote: {pack.name}{" "}
-                  {/* Substituir 'nome' pelo campo correto */}
-                  {/* Outros dados do pacote, caso necessário */}
-                  <button onClick={handleDeletePack}>Excluir</button>
+                  {isEditing ? null : (
+                    <button onClick={() => handleDeletePack(pack._id)}>
+                      Excluir
+                    </button>
+                  )}
                 </div>
               ))
             ) : (
@@ -134,5 +242,4 @@ function ProfilePage() {
     </div>
   );
 }
-
 export default ProfilePage;
